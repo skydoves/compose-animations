2371917808fa850bbeffd15f751b79a39a2b8b15
Subhrajyoti Sen <subhrajyoti12@gmail.com>
2026-05-25 15:44:00 +0200

    Animation 4: Clip before background

--- app/src/main/kotlin/com/skydoves/hotreloadanimations/animations/AnimationExample4.kt
+++ app/src/main/kotlin/com/skydoves/hotreloadanimations/animations/AnimationExample4.kt
@@ -38,6 +38,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.rotate
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
@@ -92,6 +93,7 @@ fun AnimationExample4() {
         modifier = Modifier
           .size(size)
           .rotate(rotation)
+          .clip(RoundedCornerShape(cornerDp))
           .background(color = color, shape = RoundedCornerShape(cornerDp))
           .clickable { morphed = !morphed },
         contentAlignment = Alignment.Center,
